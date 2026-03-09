40938b204dfe8e5e169d8397d436b2b9c3a1feef
pelukes <pe.lukes@gmail.com>
2026-03-09 14:51:25 +0100

    Update app.py

--- app.py
+++ app.py
@@ -144,7 +144,7 @@ with st.spinner(f"🛰️ Načítám vrstvu: {TARGETS[selected_key]['name']}..."
 # 2. Přidání vektorové PMTiles vrstvy (pokud je aktivována)
     if show_ndsm_vector:
         # Replace the URL below with your actual R2.dev subdomain URL
-        base_url = "https://pub-ddf1e6086fe44d9dbcdf57d66b64fef0.r2.dev/nDSM_change_NIL3.pmtiles"
+        base_url = "https://pub-ddf1e6086fe44d9dbcdf57d66b64fef0.r2.dev/nDSM_change_NIL3_fixed.pmtiles"
         pmtiles_url = f"{base_url}?nocache={int(time.time())}"
         
         maplibre_style = {
@@ -236,6 +236,7 @@ if map_output and map_output.get("last_clicked"):
                     st.metric(label=v["name"], value="Mimo lesní masku", delta="Žádná data")
 else:
     st.info("👆 Klikněte do mapy na libovolný zalesněný pixel pro zobrazení lokálních parametrů.")
+
 
 
 
